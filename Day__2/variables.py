6af333064823c276a7d93660a3935345417da01c
Gerard Riordan <gerardriordan@gmail.com>
2025-09-10 10:31:31 +0100

    Committing new options_calculator file first version

---
+++ Day__2/variables.py
@@ -0,0 +1,22 @@
+#Day 2 : 30 Days of python programming
+firstname = 'Gerard'
+lastname = 'Riordan'
+fullname = 'Gerard Riordan'
+country = 'Australia'
+print(firstname, lastname)
+print(len(firstname))
+
+if len(firstname) > len(lastname):
+    print('First')
+else:
+    print('Last')
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+radius = input('Radius of circle is ')
+area_of_circle = 3.14*int(radius)**2
+circum_of_circle = 2*3.14*int(radius)
+print(area_of_circle)
+print(circum_of_circle)
---
+++ helloworld.py
@@ -0,0 +1,24 @@
+print('hello')
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(15))          # Int
+print(type(3.414))        # Float
+print(type(1 - 3j))      # Complex number
+print(type('Gerard'))  # String
+print(type([1, 4, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 12, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+# Finding the euclidian distance between two points (2,3) and (10,8)
+distance = ((10-2)**2 + (8-3)**2)**0.5
+print(distance)
