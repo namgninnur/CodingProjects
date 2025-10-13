0d3b519b56b19a6b5d5c1ad4f78afb73a4a01504
namgninnur <37106037+namgninnur@users.noreply.github.com>
2025-10-13 12:29:39 +0100

    More Day14 work

--- 30DaysOfPython/day_14/higher_order_functions.py
+++ 30DaysOfPython/day_14/higher_order_functions.py
@@ -1,3 +1,4 @@
+#Functions in Python can handle using other functions as parameters
 def sum_numbers(nums):
     return sum(nums)
 
@@ -6,3 +7,121 @@ def higher_order_function(f,lst):
     return summation
 result = higher_order_function(sum_numbers,[1,2,3,4,5])
 print(result)
+
+def square(x):          # a square function
+    return x ** 2
+
+def cube(x):            # a cube function
+    return x ** 3
+
+def absolute(x):        # an absolute value function
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type): # a higher order function returning a function
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+    
+result = higher_order_function('square')
+print(result(-3))
+result = higher_order_function('cube')
+print(result(-3))
+result = higher_order_function('absolute')
+print(result(-3))
+
+#Closures
+def add_ten():
+    ten = 10
+    def add(num):
+        return num + ten
+    return add
+
+closure_result = add_ten()
+print(closure_result(5))  # 15
+print(closure_result(10))  # 20
+
+#Decorators
+
+# Normal function
+def greeting():
+    return 'Welcome to Python'
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    return wrapper
+g = uppercase_decorator(greeting)
+print(g())          # WELCOME TO PYTHON
+
+## Let us implement the example above with a decorator
+
+'''This decorator function is a higher order function
+that takes a function as a parameter'''
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    return wrapper
+@uppercase_decorator
+def greeting():
+    return 'Welcome to Python'
+print(greeting())   # WELCOME TO PYTHON
+
+#Lambda functions - Map
+
+numbers = [1, 2, 3, 4, 5] # iterable
+def square(x):
+    return x ** 2
+numbers_squared = map(square, numbers)
+print(list(numbers_squared))    # [1, 4, 9, 16, 25]
+# Lets apply it with a lambda function
+numbers_squared = map(lambda x : x ** 2, numbers)
+print(list(numbers_squared))    # [1, 4, 9, 16, 25]
+
+#Lambda functions - filter
+
+# Lets filter only even nubers
+numbers = [1, 2, 3, 4, 5]  # iterable
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+even_numbers = filter(is_even, numbers)
+print(list(even_numbers))       # [2, 4]
+
+#Lambda - Reduce
+
+numbers_str = ['1', '2', '3', '4', '5']  # iterable
+def add_two_nums(x, y):
+    return int(x) + int(y)
+
+total = reduce(add_two_nums, numbers_str)
+print(total)    # 15
+
+#EXERCISES
+#Data
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#Level 1
+#Q1 - Explain the difference between map, filter and reduce. 
+# Reduce consumes an interable and function and returns a single number.
+# Filter consumes an interabel and a function and returns an iterable
+# Map conumes an interable and a function
+
+#Q2 - Explain the difference between HOF, closure and decorator
+# HOF is a function that contains another function
+# Closures are functions which return an outcome from the inner function in the outr function
+# Decorators are functions that add new functionality to an existing object without modifying its structure
+
