07b013e59ad506d4415ffd3a9436852198a3d4f4
namgninnur <37106037+namgninnur@users.noreply.github.com>
2025-10-13 12:31:37 +0100

    Minor tweaks

--- 30DaysOfPython/day_14/higher_order_functions.py
+++ 30DaysOfPython/day_14/higher_order_functions.py
@@ -101,6 +101,8 @@ print(list(even_numbers))       # [2, 4]
 
 #Lambda - Reduce
 
+from functools import reduce
+
 numbers_str = ['1', '2', '3', '4', '5']  # iterable
 def add_two_nums(x, y):
     return int(x) + int(y)
