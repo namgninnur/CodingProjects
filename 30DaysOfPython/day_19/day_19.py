ffd0ddea4d944f7d3907605c0f8b8e236d8762d9
namgninnur <37106037+namgninnur@users.noreply.github.com>
2025-10-28 21:46:42 +0000

    More Day_19 work

--- 30DaysOfPython/day_19/day_19.py
+++ 30DaysOfPython/day_19/day_19.py
@@ -6,5 +6,43 @@
 #"t" - text
 #"b" - binary
 
-f = open('./day_19/reading_file_example.txt')
-print(f)
+#Open and read txt file
+f = open('./30DaysOfPython/day_19/reading_file_example.txt')
+txt = f.read()
+print(type(txt))
+print(txt)
+f.close()
+
+#Read first 10 chars
+f = open('./30DaysOfPython/day_19/reading_file_example.txt')
+txt2 = f.read(10)
+print(type(txt2))
+print(txt2)
+f.close()
+
+#Only read first line
+f = open('./30DaysOfPython/day_19/reading_file_example.txt')
+txt3 = f.readline()
+print(type(txt3))
+print(txt3)
+f.close()
+
+#Read all the text line by line
+f = open('./30DaysOfPython/day_19/reading_file_example.txt')
+txt4 = f.readlines()
+print(type(txt4))
+print(txt4)
+f.close()
+
+#Or can use splitlines()
+f = open('./30DaysOfPython/day_19/reading_file_example.txt')
+txt5 = f.read().splitlines()
+print(type(txt5))
+print(txt5)
+f.close()
+
+#Can also open files using 'with' which closes files by itself
+with open('./30DaysOfPython/day_19/reading_file_example.txt') as f:
+    lines = f.read().splitlines()
+    print(type(lines))
+    print(lines)
