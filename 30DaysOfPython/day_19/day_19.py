e48efe97f347ac0dd92e63f248da0bf09d74a8b6
namgninnur <37106037+namgninnur@users.noreply.github.com>
2025-11-03 10:43:19 +0000

    day_19 re file opening, changing, writing

--- 30DaysOfPython/day_19/day_19.py
+++ 30DaysOfPython/day_19/day_19.py
@@ -46,3 +46,98 @@ with open('./30DaysOfPython/day_19/reading_file_example.txt') as f:
     lines = f.read().splitlines()
     print(type(lines))
     print(lines)
+
+#Updating files
+#'a' = append to end of file
+#'w' = write - overwrites existing content, creates file if it doesnt exist
+
+with open('./30DaysOfPython/day_19/writing_file_example.txt','a') as f:
+    f.write('Doc doesnt exist so new doc with this text to be created')
+
+#Deleting files
+import os
+if os.path.exists('./30DaysOfPython/day_19/example.txt'):
+    os.remove('./30DaysOfPython/day_19/example.txt')
+else:
+    print('The file does not exist')
+
+#File Types
+#.txt covered above
+#.json = JavaScript Object Notation aka 'a stringified JavaScript object or Python dictionary
+#example
+
+# dictionary
+person_dct= {
+    "name":"Asabeneh",
+    "country":"Finland",
+    "city":"Helsinki",
+    "skills":["JavaScrip", "React","Python"]
+}
+# JSON: A string form a dictionary
+person_json = "{'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki', 'skills': ['JavaScrip', 'React', 'Python']}"
+
+# we use three quotes and make it multiple line to make it more readable
+person_json2 = '''{
+    "name":"Beyonce",
+    "country":"France",
+    "city":"Bordeaux",
+    "skills":["Singing", "Dancing","Acting"]
+}'''
+
+#Change JSON to Dictionary
+import json
+person_dct2 = json.loads(person_json2)
+print(type(person_dct2))
+print(person_dct2)
+print(person_dct2['name'])
+
+#Change dictionary to JSON
+import json
+person3 = {
+    "name": "Tom",
+    "country": "America",
+    "city": "Los Angeles",
+    "skills": ["flying","jumping","running"]
+}
+
+person_json3 = json.dumps(person3, indent = 2)
+print(type(person_json3))
+print(person_json3)
+
+#saving as JSON
+with open('./30DaysOfPython/day_19/json_example.json','w',encoding='utf-8') as f:
+    json.dump(person3, f, ensure_ascii=False, indent=2)
+
+#CSV files
+import csv
+with open('./30DaysOfPython/day_19/csv_example.csv','w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(['Text','Type','Count'])
+    csvwriter.writerow(['Chocolate','snack',5])
+    csvwriter.writerow(['Cookies','snack',7])
+    csvwriter.writerow(['Apple','fruit',2])
+
+with open('./30DaysOfPython/day_19/csv_example.csv') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are :{", ".join(row)}')
+            line_count += 1
+        else:
+            print(
+                f'\t{row[0]} is a foodstuff. It is a {row[1]} and I have {row[2]} left.')
+            line_count += 1
+    print(f'Number of lines: {line_count}')
+
+#XLSX
+#revisit after learning pip
+
+#import xlrd
+#excel_book = xlrd.open_workbook('sample.xls')
+#print(excel_book.nsheets)
+#print(excel_book.sheet_names)
+
+#Exercises - Day_19
+
+#1a)
